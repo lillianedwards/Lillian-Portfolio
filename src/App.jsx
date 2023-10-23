8cdb6d0655b540f6a2888d0c2f66c679ca14881e
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-10-23 16:45:13 -0500

    Build router and file structure

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+import About from "./views/About";
+import Contact from "./views/Contact";
+import Error from "./views/Error";
+import Resume from "./views/Resume";
+import Work from "./views/Work";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+
+
+function App() {
+
+  return (
+  <Router>
+    <Navbar/>
+    <Routes>
+    <Route path = '/' element = {<About/>}/>
+    <Route path = '/about' element = {<About/>}/>
+    <Route path = '/contact' element = {<Contact/>}/>
+    <Route path = '/resume' element = {<Resume/>}/>
+    <Route path = '/work' element = {<Work/>}/>
+    <Route path = '*' element = {<Error/>}/>
+    </Routes>
+    <Footer/>
+  </Router>
+  )
+}
+
+export default App
